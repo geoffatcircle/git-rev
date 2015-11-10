7872cbca24002c14853b3ffade47461706461305
Geoff Gaudreault <geoff@meetcircle.com>
2015-11-10 14:52:22 -0800

    added buildNumber method

--- index.js
+++ index.js
@@ -21,9 +21,31 @@ module.exports = {
   , tag : function () { 
       return _command('git describe --always --tag --abbrev=0');
     }
-  , log : function (cb) { 
+  , log : function () { 
         var str = JSON.parse(_command('git log --no-color --pretty=format:\'[ "%H", "%s", "%cr", "%an" ],\' --abbrev-commit'));
         str = str.substr(0, str.length-1)
         return JSON.parse('[' + str + ']');
+    }
+  , buildNumber : function () {
+      
+        var git_tag = this.tag().trim();
+        var git_long = this.long().trim();
+        var git_short = this.short().trim();
+        var git_branch = this.branch().trim();
+
+        var build_version = '';
+
+        if (git_tag != git_long) {
+            build_version += git_tag;
+        } else {
+            build_version += git_branch;
+        }
+
+        build_version += '.' + git_short;
+
+        var now = new Date();
+
+        build_version += " -- " + now;
+          
     }
 }
